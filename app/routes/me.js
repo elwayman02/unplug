d7206b8dcfdd0e27222ffb71bb403775c6ee8fa5
Jordan Hawker <hawker.jordan@gmail.com>
2015-09-20 19:40:18 -0700

    Improve navigation

--- app/routes/me.js
+++ app/routes/me.js
@@ -4,6 +4,12 @@ import Playlists from 'unplug/models/playlists';
 export default Ember.Route.extend({
     playlists: Playlists.create(),
 
+    afterModel() {
+        if (Ember.isPresent(Object.keys(this.get('playlists')))) {
+            this.transitionTo('me.playlists');
+        }
+    },
+
     model(params) {
         return this.get('playlists').findBy('id', params.playlistId);
     }
--- app/routes/me/playlist/index.js
+++ app/routes/me/playlist/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    setupController(controller, model) {
+        this._super.apply(this, arguments);
+        if (Ember.isPresent(model.tracks)) {
+            this.transitionTo('me.playlist.tracks', model.id);
+        }
+    }
 });
