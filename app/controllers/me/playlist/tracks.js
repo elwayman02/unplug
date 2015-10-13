d700f7561fe0515e05767be0c98e91f9460990d2
Jordan Hawker <hawker.jordan@gmail.com>
2015-10-13 11:30:54 -0700

    Add track sorting

--- app/controllers/me/playlist/tracks.js
+++ app/controllers/me/playlist/tracks.js
@@ -1,3 +1,26 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({});
+export default Ember.Controller.extend({
+    sortBy: [],
+
+    tracks: Ember.computed.sort('model.tracks', 'sortBy'),
+
+    setSortBy(sort) {
+        let dir = 'asc';
+        const sortBy = this.get('sortBy')[0];
+        if (Ember.isEqual(sortBy, `${sort}:asc`)) {
+            dir = 'desc';
+        }
+        this.set('sortBy', [`${sort}:${dir}`]);
+    },
+
+    actions: {
+        sortByArtist() {
+            this.setSortBy('author');
+        },
+
+        sortByTitle() {
+            this.setSortBy('title');
+        }
+    }
+});
